a8d1981b7ff97b8d7701313c078248f504328af9
alexeykoh <alexeykoh@icloud.com>
2025-12-13 18:27:17 +0300

    refactor: улучшена структура управления модулями, заменена типизация и добавлена инициализация модулей через реестр

--- src/core/modules/module_manager.ts
+++ src/core/modules/module_manager.ts
@@ -3,29 +3,36 @@ import BaseModule from './base_module';
 import KekModule from './base_modules/kek_module';
 import MapModule from './map_module/map_module';
 
+// названия модулей с их классами
+const moduleRegistry = {
+  KekModule: KekModule,
+  MapModule: MapModule,
+} as const;
+
+// названия модулей
+type ModuleName = keyof typeof moduleRegistry;
+
 export class ModuleManager {
   private scene!: Phaser.Scene;
   private eventBus!: EventBus;
   private baseModuleApi: Map<string, BaseModule> = new Map();
-  private baseModules: (typeof BaseModule)[] = [KekModule, MapModule];
+  private baseModules = moduleRegistry;
 
   constructor(scene: Phaser.Scene, eventBus: EventBus) {
     this.scene = scene;
     this.eventBus = eventBus;
   }
 
+  // инициализация модулей в порядке очереди
   public initBaseModules(): void {
-    this.baseModules.forEach((module) => {
-      this.baseModuleApi.set(module.name, new module(this.scene, this.eventBus));
+    Object.entries(this.baseModules).forEach(([name, ModuleClass]) => {
+      this.baseModuleApi.set(name, new ModuleClass(this.scene, this.eventBus));
     });
   }
 
-  public getModuleApi(moduleName: string): BaseModule | undefined {
+  // получить модуль по названию
+  public getModuleApi(moduleName: ModuleName): BaseModule | undefined {
     return this.baseModuleApi.get(moduleName);
-  }
-
-  public getModulesNameList(): Array<string> {
-    return [...this.baseModuleApi.keys()];
   }
 }
 
